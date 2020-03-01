c5891be1c1191658eb0fa68b57b5bfae8d02b4a9
baobaotql <40971441+baobaotql@users.noreply.github.com>
2020-03-01 11:08:16 +0800

    Update BGImage.py
    MOG获得背景

--- CV_work1/BGImage.py
+++ CV_work1/BGImage.py
@@ -15,7 +15,6 @@ while True:
     if ret is True:
         fgmask = mog.apply(image)
         ret, binary = cv.threshold(fgmask, 220, 255, cv.THRESH_BINARY)
-
         binary = cv.morphologyEx(binary, cv.MORPH_OPEN, se)
         bgimage = mog.getBackgroundImage()
         #save backgroud image
