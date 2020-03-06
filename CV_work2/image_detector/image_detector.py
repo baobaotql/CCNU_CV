19b33c7b751b58d389cd2c88ad0bb1f38cc102de
baobao <baobaotql@163.com>
2020-03-06 18:46:34 +0800

    update work2

--- CV_work2/image_detector/image_detector.py
+++ CV_work2/image_detector/image_detector.py
@@ -11,8 +11,6 @@ import cv2
 import numpy as np
 from skimage import io
 
-
-# 指定图片的人脸识别然后存储
 img = cv2.imread("test1.jpg")
 color = (0, 255, 0)
 
@@ -21,10 +19,12 @@ grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 classfier = cv2.CascadeClassifier("D:\\github_baobaotql\\CCNU_CV\\CV_work2\\image_detector\\haarcascade_frontalface_default.xml")
 faceRects = classfier.detectMultiScale(grey, scaleFactor=1.2, minNeighbors=3, minSize=(32, 32))
 
-if len(faceRects) > 0: # 大于0则检测到人脸
-    for faceRect in faceRects: # 单独框出每一张人脸
+# 大于0则检测到人脸
+if len(faceRects) > 0:
+    # 单独框出每一张人脸
+    for faceRect in faceRects:
      x, y, w, h = faceRect
-     cv2.rectangle(img, (x - 10, y - 10), (x + w + 10, y + h + 10), color, 3) #5控制绿色框的粗细
+     cv2.rectangle(img, (x - 10, y - 10), (x + w + 10, y + h + 10), color, 3)
 
 faces = classfier.detectMultiScale(grey,1.2,2)
 print("检测到人脸 ： ",len(faces))
