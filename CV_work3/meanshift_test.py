8908c226f8c4e78e77d305e318f74a89615b6ab3
baobao <baobaotql@163.com>
2020-04-03 19:12:53 +0800

    Update meanshift_test.py

--- CV_work3/meanshift_test.py
+++ CV_work3/meanshift_test.py
@@ -19,7 +19,7 @@ track_window = (c, r, w, h)
 
 roi = frame[r: r + h, c: c + w]
 hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 2556., 255.)))
+mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
 roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
 cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 
